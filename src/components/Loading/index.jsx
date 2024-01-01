94de46c5880f674d3eabf2c5896e8fc356d280ad
Mehedi Hasan <thecodermehedi@gmail.com>
2024-01-01 10:26:38 +0600

    Add Loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import FadeLoader from "react-spinners/FadeLoader";
+const Loading = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <FadeLoader />
+    </section>
+  );
+};
+
+export default Loading;
